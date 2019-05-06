92a7ffee0168295a3d6d0e357982c53aac182a30
marinatuji <mtujimura@gmail.com>
2019-05-06 15:38:30 -0300

    index.spec.js: correção do ESlint

--- test/index.spec.js
+++ test/index.spec.js
@@ -3,9 +3,9 @@ const expect = require('chai').expect;
 const cpfValidator = require('../lib/index');
 
 //representatividade do Given/When/Then
-describe("cpfValidator", () => {
+describe('cpfValidator', () => {
 
-  describe("Entrada válida do CPF", () => {
+  describe('Entrada válida do CPF', () => {
     it('Retorna true: CPF válido com máscara ', () => {
       expect(cpfValidator('338.669.038-63')).to.equal(true);
     });
@@ -16,7 +16,7 @@ describe("cpfValidator", () => {
       // expect('974.255.420-05').t;
       expect(cpfValidator('974.255.420-06')).to.be.false;
     });
-    it("Retorna false: CPF inválido com dígitos iguais", () => {
+    it('Retorna false: CPF inválido com dígitos iguais', () => {
       expect(cpfValidator(11111111111)).to.equal(false);
     });
     it('Retorna false: CPF com primeiro dígito inválido ', () => {
@@ -28,7 +28,7 @@ describe("cpfValidator", () => {
     it('Retorna erro: Caso de entrada com caracteres não compatíveis', () => {
       expect(() => { cpfValidator('abc.def.ghi-jk') }).to.throw(Error, 'Verifique número de dígitos: Esperado 11 /Encontrado 0');
     });
-    it("Retorna erro: Caso de tamanho diferente de 11", () => {
+    it('Retorna erro: Caso de tamanho diferente de 11', () => {
       expect(() => { cpfValidator(123456789) }).to.throw(Error, 'Verifique número de dígitos: Esperado 11 /Encontrado 9');
     });
     it('Retorna erro: Caso do tipo de dado incompativel (booleano)', () => {
